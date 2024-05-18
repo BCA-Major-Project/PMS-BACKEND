73c0577fd46d080217196b50fb62292b3590810c
Sneha <doyelsd123@gmail.com>
2024-05-18 20:39:21 +0530

    bug fixing

--- src/main/java/com/project/pms/controller/ProjectController.java
+++ src/main/java/com/project/pms/controller/ProjectController.java
@@ -15,9 +15,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.project.pms.dto.ProjectUsersDTO;
+import com.project.pms.model.AssignedProjects;
 import com.project.pms.model.Project;
 import com.project.pms.model.ProjectData;
 import com.project.pms.model.User;
+import com.project.pms.services.AssignedProjectsService;
 import com.project.pms.services.ProjectService;
 import com.project.pms.services.UserService;
 
@@ -32,7 +34,8 @@ public class ProjectController {
     ProjectService project_service;
     @Autowired
     UserService obj;
-    
+    @Autowired 
+    AssignedProjectsService aps_obj;
     @PostMapping("/project")
     public Project addProject(@RequestBody ProjectUsersDTO projectUsersDTO) {
         Project project = projectUsersDTO.getProject();
@@ -43,7 +46,7 @@ public class ProjectController {
 
         // Assign users to the project
         // Assuming there's a method in project_service to handle user assignments
-        // this.project_service.assignUsersToProject(users, savedProject);
+        this.aps_obj.assignUsersToProject(users, savedProject);
 
         return savedProject;
     }
--- src/main/java/com/project/pms/model/User.java
+++ src/main/java/com/project/pms/model/User.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
+// import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 
 @Entity
