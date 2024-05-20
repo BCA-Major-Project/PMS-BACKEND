3e183595b2664acc1ff2bdc88e47d1094ac84dad
Protik21 <protikb2003@gmail.com>
2024-05-20 22:02:54 +0530

    forgot password otp generate with @SHREASHREE26TECH

--- src/main/java/com/project/pms/controller/ForgotController.java
+++ src/main/java/com/project/pms/controller/ForgotController.java
@@ -1,28 +1,92 @@
 package com.project.pms.controller;
 
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import java.util.Random;
+// import org.springframework.web.bind.annotation.RequestMapping;
+
+
+// @RestController
+// @RequestMapping("/pms")
+// public class ForgotController {
+
+//     @PostMapping("/forgotpwd/{email}")
+//     public ResponseEntity<Integer> sendOTP(@PathVariable String email) {
+//         System.out.println("Received email: " + email);
+
+//         // Generate OTP of 4 digits
+//         Random random = new Random();
+//         int otp = 100000 + random.nextInt(900000); // Generate a random 4-digit OTP
+//         System.out.println("Generated OTP: " + otp);
+
+//         // Simulate sending OTP via email or SMS (not implemented in this example)
+//         return ResponseEntity.ok(otp);
+//     }
+// }
+
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import com.project.pms.services.ForgotService;
+
+// @RestController
+// public class ForgotController {
+
+//     @Autowired
+//     private ForgotService ForgotService;
+
+//     @GetMapping("/send-otp/{email}")
+//     public String sendOtp(@RequestParam String email) {
+//         String otp = ForgotController.generateOtp();
+//         ForgotService.sendOtp(email, otp);
+//         return "OTP sent to " + email;
+//     }
+
+//     private static String generateOtp() {
+//         // TODO Auto-generated method stub
+//         throw new UnsupportedOperationException("Unimplemented method 'generateOtp'");
+//     }
+// }
+
+
+// package com.project.pms.controller;
+import com.project.pms.services.EmailService;
+
+// import com.project.pms.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @RestController
 @RequestMapping("/pms")
 public class ForgotController {
 
+    @Autowired
+    private EmailService emailService;
+
     @PostMapping("/forgotpwd/{email}")
-    public ResponseEntity<Integer> sendOTP(@PathVariable String email) {
+    public ResponseEntity<String> sendOTP(@PathVariable String email) {
         System.out.println("Received email: " + email);
 
-        // Generate OTP of 4 digits
+        // Generate OTP of 6 digits
         Random random = new Random();
-        int otp = 1000 + random.nextInt(9000); // Generate a random 4-digit OTP
+        int otp = 100000 + random.nextInt(900000); // Generate a random 6-digit OTP
         System.out.println("Generated OTP: " + otp);
 
-        // Simulate sending OTP via email or SMS (not implemented in this example)
-        return ResponseEntity.ok(otp);
+        // Send OTP via email
+        emailService.sendOtp(email, otp);
+
+        return ResponseEntity.ok("OTP sent to " + email);
     }
 }
